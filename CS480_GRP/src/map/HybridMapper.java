fbebad339df491d9133628c95feef995e235ccf2
sparkiosn <shacklefurd@gmail.com>
2015-05-06 16:46:12 -0600

    Quick updates to the Hybrid Map class.

--- CS480_GRP/src/map/HybridMapper.java
+++ CS480_GRP/src/map/HybridMapper.java
@@ -6,27 +6,24 @@ import java.io.StringReader;
 
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Mapper.Context;
 
 import writable.DayStatsWritable;
 
-
 public class HybridMapper extends Mapper<Object, Text, Text, DayStatsWritable>{
-	
-	Text mapkey = new Text();
-	
+
+	private static Text mapkey = new Text();
+
 	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 
-//		DayStatsWritable valout = new DayStatsWritable(value);
-//		mapkey.set(valout.getTicker());
-//		context.write(mapkey, valout);
-
 		DayStatsWritable dayStat;
+
+		//		DayStatsWritable valout = new DayStatsWritable(value);
+		//		mapkey.set(valout.getTicker());
+		//		context.write(mapkey, valout);		
 
 		BufferedReader bufReader = new BufferedReader(new StringReader(value.toString()));
 		String line = null;
 		while( (line = bufReader.readLine()) != null ) {
-
 
 			try{
 				dayStat = new DayStatsWritable(new Text(line));
@@ -40,5 +37,6 @@ public class HybridMapper extends Mapper<Object, Text, Text, DayStatsWritable>{
 			}
 
 		}
+		
 	}
 }
