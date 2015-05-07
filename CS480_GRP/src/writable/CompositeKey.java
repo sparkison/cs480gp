61393ddd5b057b42d7fc95d5b5bb065d0ad5d9da
sparkison <shacklefurd@gmail.com>
2015-05-07 13:33:04 -0600

    CompositeKey writable class

---
+++ CS480_GRP/src/writable/CompositeKey.java
@@ -0,0 +1,101 @@
+package writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
+
+public class CompositeKey implements WritableComparable<CompositeKey> {
+
+	/**
+	 * the ticker field
+	 */
+	Text ticker;
+
+	/**
+	 * the date field
+	 */
+	Text date;
+
+	public CompositeKey() {}
+
+	public CompositeKey(Text ticker, Text date) {
+		this.ticker = ticker;
+		this.date = date;
+	}
+
+	/**
+	 * set the ticker Text
+	 * @param ticker
+	 */
+	public void setTicker(Text t) {
+		if (ticker == null)
+			ticker = new Text();
+
+		ticker.set(t);
+	}
+
+	/**
+	 * set the date text
+	 * @param date
+	 */
+	public void setDate(Text d) {
+		if (date == null)
+			date = new Text();
+
+		date.set(d);
+	}
+
+	/**
+	 * get the ticker field
+	 * 
+	 * @return the ticker field
+	 */
+	public Text getTicker() {
+		return ticker;
+	}
+
+	/**
+	 * get the date field
+	 * 
+	 * @return the date field
+	 */
+	public Text getDate() {
+		return date;
+	}
+
+	public void write(DataOutput out) throws IOException {
+		ticker.write(out);
+		date.write(out);
+	}
+
+	public void readFields(DataInput in) throws IOException {
+		if (ticker == null)
+			ticker = new Text();
+
+		if (date == null)
+			date = new Text();
+
+		ticker.readFields(in);
+		date.readFields(in);
+	}
+
+	public int compareTo(CompositeKey other) {
+		int cmp = getTicker().compareTo(other.getTicker());
+		if (cmp != 0) {
+			return cmp;
+		}
+		return this.getDate().compareTo(other.getDate()); // reverse
+	}
+
+	public int hashCode() {
+		return ticker.hashCode();
+	}
+
+	public boolean equals(Object o) {
+		CompositeKey p = (CompositeKey) o;
+		return ticker.equals(p.getTicker());
+	}
+}
